139b7795c6c6b209381ee0d16c706b24bcd3f029
danielrive <danielrivera846@gmail.com>
2023-10-30 15:10:05 +0100

    remove extra var in gh workflow

--- .github/jobs/bootstrap-flux.sh
+++ .github/jobs/bootstrap-flux.sh
@@ -2,7 +2,7 @@
 
 ### install flux
 
-echo "installing flux"
+echo "installing flux cli"
 
 curl -s https://fluxcd.io/install.sh | sudo bash
 
@@ -10,10 +10,15 @@ curl -s https://fluxcd.io/install.sh | sudo bash
 echo "get eks credentials"
 aws eks update-kubeconfig --name $CLUSTER_NAME  --region $AWS_REGION
 
+## validate if flux is installed
+
+$fluxCRDs = kubectl api-resources
+
 echo "run flux bootstrap"
 
-flux bootstrap github \
-  --owner=$GH_USER_NAME \
-  --repository=$FLUX_REPO_NAME \
-  --path="clusters/$ENVIRONMENT" \
-  --personal
+flux bootstrap github \ # manifest for flucd configs will be stored in GitHub repo
+  --owner=$GH_USER_NAME \  # Define the user name to use in GitHub
+  --repository=$FLUX_REPO_NAME \ # The Github repository name where the flux manifest will be stored
+  --path="clusters/$ENVIRONMENT" \ # The path where the flux manifest will be stored
+  --personal  # the owner is assumed to be a GitHub user not an organization
+  --branch main # Branch name 
