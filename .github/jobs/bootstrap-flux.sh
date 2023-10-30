eac4020958d4816b08f1ad63ff7dd21ead2e23e4
danielrive <danielrivera846@gmail.com>
2023-10-30 21:10:47 +0100

    fix conditional to install flux

--- .github/jobs/bootstrap-flux.sh
+++ .github/jobs/bootstrap-flux.sh
@@ -6,8 +6,7 @@ aws eks update-kubeconfig --name $CLUSTER_NAME  --region $AWS_REGION
 
 ## validate if flux is installed
 
-flux_installed=$(kubectl api-resources --api-group=flux.weave.works --no-headers)
-flux_version=
+flux_installed=$(kubectl api-resources | grep flux)
 echo $flux_installed
 if [ -z "$flux_installed" ]; then
   echo "flux is not installed"
@@ -23,8 +22,9 @@ if [ -z "$flux_installed" ]; then
     --owner=$GH_USER_NAME \
     --repository=$FLUX_REPO_NAME \
     --path="clusters/$ENVIRONMENT" \
-    --personal \
-    --branch main 
+    --branch=main \ 
+    --version=v2.1.2 \
+    --personal
 else
   echo "flux is installed"
 fi
