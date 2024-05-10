60686a82f33516fa28285ac141b24075c5f31852
danielrive <danielrivera846@gmail.com>
2024-05-10 23:46:45 +0200

    test 1st folder refactor structure

--- infra/terraform/eks-cluster/outputs.tf
+++ infra/terraform/eks-cluster/outputs.tf
@@ -1,24 +1,5 @@
 ###################################
-####### Networking outputs
-
-output "vpc_id" {
-  value = module.networking.vpc.id
-}
+####### eks outputs
 
 
-output "public_subnets" {
-  value = module.networking.main.public_subnets
-}
 
-
-output "private_subnets" {
-  value = module.networking.main.public_subnets
-}
-
-output "kms_eks_arn" {
-  value = module.kms_key_eks.kms_arn
-}
-
-output "kms_eks_id" {
-  value = module.kms_key_eks.kms_id
-}
