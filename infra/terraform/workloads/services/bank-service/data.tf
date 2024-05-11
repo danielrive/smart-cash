3ba1ab9daa8784edfd253604cb273840a2aa9f92
danielrive <danielrivera846@gmail.com>
2024-05-11 23:56:38 +0200

    full refactor

--- infra/terraform/workloads/services/bank-service/data.tf
+++ infra/terraform/workloads/services/bank-service/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
     bucket = "${var.project_name}-tf-state-lock-${var.environment}-${var.region}" 
-    key    = "base/eks-cluster.tfstate"
+    key    = "stage/eks-cluster/eks-cluster.tfstate"
     region  = var.region
   }
 }
@@ -13,7 +13,6 @@ data "terraform_remote_state" "eks" {
 data "github_repository" "flux-gitops" {
   full_name = "danielrive/smart-cash-gitops-flux"
 }
-
 
 
 ## Getting aws account ID 
