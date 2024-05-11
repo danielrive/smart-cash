d9bb283e500d8fc8b09020d3aa56f840521a1889
danielrive <danielrivera846@gmail.com>
2024-05-11 21:30:44 +0200

    refactor

--- infra/terraform/eks-cluster/data.tf
+++ infra/terraform/eks-cluster/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "base" {
   backend = "s3"
   config = {
     bucket = "${var.project_name}-tf-state-lock-${var.environment}-${var.region}" 
-    key    = "base/base.tfstate"
+    key    = "stage/base/base.tfstate"
     region  = var.region
   }
 }
