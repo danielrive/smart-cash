c16a62db025ae7c9e0e1177272af69f9a4f503ad
danielrive <danielrivera846@gmail.com>
2024-05-12 16:27:15 +0200

    fix k8 deploy

--- infra/terraform/workloads/common/outputs.tf
+++ infra/terraform/workloads/common/outputs.tf
@@ -2,26 +2,3 @@
 ####### eks outputs
 
 
-output "cluster_endpoint" {
-  value = aws_eks_cluster.kube_cluster.endpoint
-}
-
-output "cluster_oidc" {
-  value = replace(aws_eks_cluster.kube_cluster.identity[0].oidc[0].issuer, "https://", "")
-}
-
-output "cluster_name" {
-  description = "Cluster name"
-  value       = aws_eks_cluster.kube_cluster.id
-}
-
-output "cluster_security_group_id" {
-  description = "Cluster security group id"
-  value       = aws_eks_cluster.kube_cluster.vpc_config[0].cluster_security_group_id
-}
-
-output "created_at" {
-  description = "Cluster created_at"
-  value       = aws_eks_cluster.kube_cluster.created_at
-}
-
