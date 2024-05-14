72a4a9af52d96377ecda59cd260ec53597241ad4
Daniel German Rivera <danielrivera846@gmail.com>
2024-05-14 20:56:57 +0200

    Refactor folders in Gitops repo (#19)
    * Refactor GitOps repo folders to avoid problems with dependencies between k8 resources
    * Pipeline improvements

--- infra/terraform/eks-cluster/main.tf
+++ infra/terraform/eks-cluster/main.tf
@@ -1,7 +1,8 @@
 locals {
   brach_gitops_repo = "main"
-  path_tf_repo_flux_kustomization = "../../kubernetes/kustomizations"
-  path_tf_repo_flux_sources = "../../kubernetes/flux-sources"
+  path_tf_repo_flux_kustomization = "../../kubernetes/bootstrap/kustomizations"
+  path_tf_repo_flux_sources = "../../kubernetes/bootstrap/flux-sources"
+  path_tf_repo_flux_core = "../../kubernetes/core"
   path_tf_repo_flux_common = "../../kubernetes/common"
   cluster_name = "${var.project_name}-${var.environment}"
   gh_username = "danielrive"
@@ -93,8 +94,7 @@ resource "null_resource" "bootstrap-flux" {
     EOF
   }
   triggers = {
-    cluster_oidc = module.eks_cluster.cluster_oidc
-    created_at   = module.eks_cluster.created_at
+    always_run = timestamp() # this will always run
   }
 
 }
@@ -103,54 +103,15 @@ resource "null_resource" "bootstrap-flux" {
 #######    GitOps Configuration 
 ###############################################
 
-####################################
-#### Flux kustomizations bootstrap
-
-resource "github_repository_file" "kustomizations-bootstrap" {
-  depends_on          = [module.eks_cluster,null_resource.bootstrap-flux]
-  repository          = data.github_repository.flux-gitops.name
-  branch              = local.brach_gitops_repo
-  file                = "clusters/${local.cluster_name}/bootstrap/core-kustomize.yaml"
-  content = templatefile(
-    "${local.path_tf_repo_flux_kustomization}/core-kustomize.yaml",
-    {
-      CLUSTER_NAME = local.cluster_name
-    }
-  )
-  commit_message      = "Managed by Terraform"
-  commit_author       = "From terraform"
-  commit_email        = "gitops@smartcash.com"
-  overwrite_on_create = true
-}
-
-###########################
-##### Flux Sources 
-
-resource "github_repository_file" "sources" {
-  depends_on          = [module.eks_cluster,github_repository_file.kustomizations-bootstrap]
-  for_each            = fileset(local.path_tf_repo_flux_sources, "*.yaml")
-  repository          = data.github_repository.flux-gitops.name
-  branch              = local.brach_gitops_repo
-  file                = "clusters/${local.cluster_name}/core/${each.key}"
-  content = templatefile(
-    "${local.path_tf_repo_flux_sources}/${each.key}",
-    {}
-  )
-  commit_message      = "Managed by Terraform"
-  commit_author       = "From terraform"
-  commit_email        = "gitops@smartcash.com"
-  overwrite_on_create = true
-}
 
 ################################################
-##### Flux kustomizations core
-
+##### Flux kustomizations bootstrap
 resource "github_repository_file" "kustomizations" {
-  depends_on          = [module.eks_cluster,github_repository_file.sources]
+  depends_on          = [module.eks_cluster,null_resource.bootstrap-flux]
   for_each            = fileset(local.path_tf_repo_flux_kustomization, "*.yaml")
   repository          = data.github_repository.flux-gitops.name
   branch              = local.brach_gitops_repo
-  file                = "clusters/${local.cluster_name}/core/${each.key}"
+  file                = "clusters/${local.cluster_name}/bootstrap/${each.key}"
   content = templatefile(
     "${local.path_tf_repo_flux_kustomization}/${each.key}",
     {
@@ -165,15 +126,17 @@ resource "github_repository_file" "kustomizations" {
 }
 
 
-############################
-##### OPA templates
+###########################
+##### Flux Sources 
 
-resource "github_repository_file" "opa_templates" {
+resource "github_repository_file" "sources" {
+  depends_on          = [module.eks_cluster,github_repository_file.kustomizations]
+  for_each            = fileset(local.path_tf_repo_flux_sources, "*.yaml")
   repository          = data.github_repository.flux-gitops.name
   branch              = local.brach_gitops_repo
-  file                = "clusters/${local.cluster_name}/core/opa-templates.yaml"
+  file                = "clusters/${local.cluster_name}/bootstrap/${each.key}"
   content = templatefile(
-    "../../kubernetes/opa-policies/opa-templates.yaml",
+    "${local.path_tf_repo_flux_sources}/${each.key}",
     {}
   )
   commit_message      = "Managed by Terraform"
@@ -183,13 +146,63 @@ resource "github_repository_file" "opa_templates" {
 }
 
 
+###########################
+##### Core resources
+
+resource "github_repository_file" "core_resources" {
+  depends_on          = [module.eks_cluster,null_resource.bootstrap-flux]
+  for_each            = fileset(local.path_tf_repo_flux_core, "*.yaml")
+  repository          = data.github_repository.flux-gitops.name
+  branch              = local.brach_gitops_repo
+  file                = "clusters/${local.cluster_name}/core/${each.key}"
+  content = templatefile(
+    "${local.path_tf_repo_flux_core}/${each.key}",
+    {
+      ## Common variables for manifests
+      AWS_REGION = var.region
+      ENVIRONMENT = var.environment
+      PROJECT = var.project_name    
+      ARN_CERT_MANAGER_ROLE = "arn:aws:iam::12345678910:role/cert-manager-us-west-2"    
+    }
+  )
+  commit_message      = "Managed by Terraform"
+  commit_author       = "From terraform"
+  commit_email        = "gitops@smartcash.com"
+  overwrite_on_create = true
+}
+
+###########################
+##### Common resources
+
+resource "github_repository_file" "common_resources" {
+  depends_on          = [module.eks_cluster,github_repository_file.core_resources]
+  for_each            = fileset(local.path_tf_repo_flux_common, "*.yaml")
+  repository          = data.github_repository.flux-gitops.name
+  branch              = local.brach_gitops_repo
+  file                = "clusters/${local.cluster_name}/common/${each.key}"
+  content = templatefile(
+    "${local.path_tf_repo_flux_common}/${each.key}",
+    {
+      ## Common variables for manifests
+      AWS_REGION = var.region
+      ENVIRONMENT = var.environment
+      PROJECT = var.project_name
+    }
+  )
+  commit_message      = "Managed by Terraform"
+  commit_author       = "From terraform"
+  commit_email        = "gitops@smartcash.com"
+  overwrite_on_create = true
+}
+
 ############################
 ##### OPA constraints
 
 resource "github_repository_file" "opa_constraints" {
+  depends_on          = [module.eks_cluster,github_repository_file.common_resources]
   repository          = data.github_repository.flux-gitops.name
   branch              = local.brach_gitops_repo
-  file                = "clusters/${local.cluster_name}/opa-policies/opa-templrates.yaml"
+  file                = "clusters/${local.cluster_name}/opa-policies/opa-constraints.yaml"
   content = templatefile(
     "../../kubernetes/opa-policies/constraints.yaml",
     {
