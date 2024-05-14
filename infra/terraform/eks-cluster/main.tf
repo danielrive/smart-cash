4ae3bf3378bd89186cf795676cac3407b0a42be7
danielrive <danielrivera846@gmail.com>
2024-05-14 10:39:09 +0200

    migrate common k8 resources creation to eks tf folder

--- infra/terraform/eks-cluster/main.tf
+++ infra/terraform/eks-cluster/main.tf
@@ -165,13 +165,42 @@ resource "github_repository_file" "kustomizations" {
 }
 
 
+###########################
+##### Common resources
+
+resource "github_repository_file" "common_resources" {
+  for_each            = fileset(local.path_tf_repo_flux_common, "*.yaml")
+  repository          = data.github_repository.flux-gitops.name
+  branch              = local.brach_gitops_repo
+  file                = "clusters/${local.cluster_name}/common/${each.key}"
+  content = templatefile(
+    "${local.path_tf_repo_flux_common}/${each.key}",
+    {
+      ## Common variables for manifests
+      AWS_REGION = var.region
+      ENVIRONMENT = var.environment
+      PROJECT = var.project_name
+      ## Variables cert manager
+      ARN_CERT_MANAGER_ROLE = "arn:aws:iam::12345678910:role/cert-manager-us-west-2"
+      ## Variables for Grafana
+      ## Variables for ingress
+      
+    }
+  )
+  commit_message      = "Managed by Terraform"
+  commit_author       = "From terraform"
+  commit_email        = "gitops@smartcash.com"
+  overwrite_on_create = true
+}
+
+
 ############################
 ##### OPA templates
 
 resource "github_repository_file" "opa_templates" {
   repository          = data.github_repository.flux-gitops.name
   branch              = local.brach_gitops_repo
-  file                = "clusters/${local.cluster_name}/core/opa-templates.yaml"
+  file                = "clusters/${local.cluster_name}/common/opa-templates.yaml"
   content = templatefile(
     "../../kubernetes/opa-policies/opa-templates.yaml",
     {}
@@ -189,7 +218,7 @@ resource "github_repository_file" "opa_templates" {
 resource "github_repository_file" "opa_constraints" {
   repository          = data.github_repository.flux-gitops.name
   branch              = local.brach_gitops_repo
-  file                = "clusters/${local.cluster_name}/opa-policies/opa-templrates.yaml"
+  file                = "clusters/${local.cluster_name}/opa-policies/opa-constraints.yaml"
   content = templatefile(
     "../../kubernetes/opa-policies/constraints.yaml",
     {
