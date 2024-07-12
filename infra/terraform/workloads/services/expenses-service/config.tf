c31fa32bbb677b9f44e9cbd9f9e2fc09ac49de2b
danielrive <danielrivera846@gmail.com>
2024-07-12 10:59:10 +0200

    refactor destroy process

--- infra/terraform/workloads/services/expenses-service/config.tf
+++ infra/terraform/workloads/services/expenses-service/config.tf
@@ -2,15 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 5.41.0"
+      version = "~> 5.57.0"
     }  
     github = {
       source  = "integrations/github"
-      version = "~> 6.1.0"
+      version = "~> 6.2.3"
     }
   }
 }
-
 provider "aws" {
   region = var.region
   default_tags {
