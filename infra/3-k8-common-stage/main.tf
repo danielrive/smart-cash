e274441694edd78a1fb662a51260420ddcdcf1c1
danielrive <danielrivera846@gmail.com>
2024-08-26 23:40:38 +0200

    argo

--- infra/3-k8-common-stage/main.tf
+++ infra/3-k8-common-stage/main.tf
@@ -1,21 +1,42 @@
 locals {
   brach_gitops_repo        = var.environment
-  path_tf_repo_flux_common = "./k8-manifests/common"
   cluster_name             = "${var.project_name}-${var.environment}"
-  gh_username              = "danielrive"
 }
 
+
+### Common ArgoApps 
+
+resource "github_repository_file" "common_argo_apps" {
+  for_each   = fileset("./k8-manifests/argo-apps", "*.yaml")
+  repository = data.github_repository.gh_gitops.name
+  branch     = local.brach_gitops_repo
+  file       = "clusters/${local.cluster_name}/bootstrap/${each.key}"
+  content = templatefile(
+    "./k8-manifests/argo-apps/${each.key}",
+    {
+      ## Common variables for manifests
+      ENVIRONMENT           = var.environment
+      REPO_URL = data.github_repository.gh_gitops.http_clone_url
+      GITOPS_PATH_COMMON = "clusters/${local.cluster_name}/common"
+    }
+  )
+  commit_message      = "Managed by Terraform"
+  commit_author       = "From terraform"
+  commit_email        = "gitops@smartcash.com"
+  overwrite_on_create = true
+}
 
 ###########################
 #### Common resources
 
 resource "github_repository_file" "common_resources" {
+  depends_on = [github_repository_file.common_argo_apps]
   for_each   = fileset(local.path_tf_repo_flux_common, "*.yaml")
   repository = data.github_repository.flux-gitops.name
   branch     = local.brach_gitops_repo
   file       = "clusters/${local.cluster_name}/common/${each.key}"
   content = templatefile(
-    "${local.path_tf_repo_flux_common}/${each.key}",
+    "./k8-manifests/common/${each.key}",
     {
       ## Common variables for manifests
       AWS_REGION  = var.region
@@ -29,6 +50,7 @@ resource "github_repository_file" "common_resources" {
   overwrite_on_create = true
 }
 
+/*
 #################################
 ##### OPA constraints(policies)
 
@@ -47,3 +69,4 @@ resource "github_repository_file" "opa_constraints" {
   commit_email        = "gitops@smartcash.com"
   overwrite_on_create = true
 }
+*/
