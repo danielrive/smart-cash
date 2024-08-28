93ed31e6d058c8ad1d233e4bee4ca5fe783fb8bc
danielrive <danielrivera846@gmail.com>
2024-08-28 22:12:29 +0200

    auto-sync main app

--- infra/4-workloads-stage/frontend-service/frontend-service.tf
+++ infra/4-workloads-stage/frontend-service/frontend-service.tf
@@ -150,25 +150,3 @@ resource "github_repository_file" "network_policy" {
   commit_email        = "gitops@smartcash.com"
   overwrite_on_create = true
 }
-
-
-##### Images Updates automation
-resource "github_repository_file" "image_updates" {
-  for_each   = fileset("${local.path_tf_repo_services}/flux-image-update", "*.yaml")
-  repository = data.github_repository.gh_gitops.name
-  branch     = local.brach_gitops_repo
-  file       = "clusters/${local.cluster_name}/services/${local.this_service_name}-service/base/${each.key}"
-  content = templatefile(
-    "${local.path_tf_repo_services}/flux-image-update/${each.key}",
-    {
-      SERVICE_NAME    = local.this_service_name
-      ECR_REPO        = module.ecr_registry.repo_url
-      ENVIRONMENT     = var.environment
-      PATH_DEPLOYMENT = "services/${local.this_service_name}-service/overlays/${var.environment}/kustomization.yaml"
-    }
-  )
-  commit_message      = "Managed by Terraform"
-  commit_author       = "From terraform"
-  commit_email        = "gitops@smartcash.com"
-  overwrite_on_create = true
-}
