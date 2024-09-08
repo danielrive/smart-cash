def940231119d17a65c84464a4a7ae4542ae10fe
danielrive <danielrivera846@gmail.com>
2024-09-08 17:53:49 +0200

    added aws pod identity

--- infra/2-eks-cluster-stage/main.tf
+++ infra/2-eks-cluster-stage/main.tf
@@ -34,9 +34,9 @@ module "eks_cluster" {
   key_pair_name              = "k8-admin"
   instance_type_worker_nodes = var.environment == "develop" ? "t3.medium" : "t3.medium"
   AMI_for_worker_nodes       = "AL2_x86_64"
-  desired_nodes              = 2
-  max_instances_node_group   = 2
-  min_instances_node_group   = 2
+  desired_nodes              = 3
+  max_instances_node_group   = 3
+  min_instances_node_group   = 3
   storage_nodes              = 20
 }
 
