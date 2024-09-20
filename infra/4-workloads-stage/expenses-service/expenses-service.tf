e69296bf2853d262061adc4920bf5cb036ce452a
danielrive <danielrivera846@gmail.com>
2024-09-20 22:22:45 +0200

    reemplace kustomization image update

--- infra/4-workloads-stage/expenses-service/expenses-service.tf
+++ infra/4-workloads-stage/expenses-service/expenses-service.tf
@@ -213,6 +213,9 @@ resource "github_repository_file" "overlays_svc_kustomization" {
   commit_author       = "From terraform"
   commit_email        = "gitops@smartcash.com"
   overwrite_on_create = true
+  lifecycle {
+    ignore_changes = [content]
+  }
 }
 
 ##### Network Policies
