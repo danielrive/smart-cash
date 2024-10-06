4015688e6576caa31ad8648dfa290b1a27f68abc
danielrive <danielrivera846@gmail.com>
2024-10-06 12:47:13 +0200

    added cloudwatch addon

--- infra/2-eks-cluster-stage/bootstrap-flux.sh
+++ infra/2-eks-cluster-stage/bootstrap-flux.sh
@@ -11,7 +11,7 @@
 echo "---------->  get eks credentials"
 aws eks update-kubeconfig --name $1  --region $2
 
-if [[ "$5"=="production"  ]]; then
+if [ "$5"=="production"  ]; then
   BRANCH="main"
 else 
   BRANCH=$5
@@ -20,7 +20,7 @@ fi
 
 flux_installed=$(kubectl api-resources | grep flux)
 echo "---> $flux_installed"
-if [[ -z "$flux_installed" ]]; then
+if [ -z "$flux_installed" ]; then
   echo "---------->  flux is not installed"
 
   ### install flux
