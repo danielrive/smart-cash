f5ca0b2f7859e073104990e8655efce960a60c96
danielrive <danielrivera846@gmail.com>
2024-11-22 16:35:00 +0100

    fix

--- app/utils/otelInit.go
+++ app/utils/otelInit.go
@@ -24,8 +24,8 @@ func InitOpenTelemetry(otelUrl string, serviceName string, logger *slog.Logger) 
 		context.Background(),
 		resource.WithFromEnv(),      // Discover and provide attributes from OTEL_RESOURCE_ATTRIBUTES and OTEL_SERVICE_NAME environment variables.
 		resource.WithTelemetrySDK(), // Discover and provide information about the OpenTelemetry SDK used.
-		resource.WithProcess(),      // Discover and provide process information.
-		resource.WithContainer(),    // Discover and provide container information.
+		//resource.WithProcess(),      // Discover and provide process information.
+		//resource.WithContainer(),    // Discover and provide container information.
 		//resource.WithHost(),         // Discover and provide host information.
 		resource.WithAttributes(attribute.String("serviceName", serviceName)), // Add custom resource attributes.
 		// resource.WithDetectors(thirdparty.Detector{}), // Bring your own external Detector implementation.
