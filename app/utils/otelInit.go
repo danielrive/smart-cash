fab23bb9f8485be93b07f19875ec60b83f79236d
danielrive <danielrivera846@gmail.com>
2024-11-22 17:05:12 +0100

    fix

--- app/utils/otelInit.go
+++ app/utils/otelInit.go
@@ -7,10 +7,10 @@ import (
 	"log/slog"
 	"time"
 
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
 // Set up openTelemetry
@@ -25,9 +25,9 @@ func InitOpenTelemetry(otelUrl string, serviceName string, logger *slog.Logger) 
 		resource.WithFromEnv(),      // Discover and provide attributes from OTEL_RESOURCE_ATTRIBUTES and OTEL_SERVICE_NAME environment variables.
 		resource.WithTelemetrySDK(), // Discover and provide information about the OpenTelemetry SDK used.
 		//resource.WithProcess(),      // Discover and provide process information.
-		//resource.WithContainer(),    // Discover and provide container information.
+		resource.WithContainer(), // Discover and provide container information.
 		//resource.WithHost(),         // Discover and provide host information.
-		resource.WithAttributes(attribute.String("ServiceName", serviceName)), // Add custom resource attributes.
+		resource.WithAttributes(semconv.ServiceNameKey.String(serviceName)), // Add custom resource attributes.
 		// resource.WithDetectors(thirdparty.Detector{}), // Bring your own external Detector implementation.
 	)
 
