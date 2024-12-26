71acc0ec2fda9ffe13ce130dcd56be2cf50e2ff3
danielrive <danielrivera846@gmail.com>
2024-12-25 19:28:21 -0500

    fix karpenter version in url

--- infra/modules/karpenter-core/main.tf
+++ infra/modules/karpenter-core/main.tf
@@ -4,7 +4,7 @@ resource "aws_cloudformation_stack" "network" {
   parameters = {
     ClusterName = var.cluster_name
   }
-  template_body = file("./cloudformation.yaml")
+  template_body = file("${path.module}/cloudformation.yaml")
   capabilities = ["CAPABILITY_NAMED_IAM"]
 
 }
