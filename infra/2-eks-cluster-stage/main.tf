b0b9b39cb832ed47f579361d5a5529abc02f1055
danielrive <danielrivera846@gmail.com>
2025-01-10 16:29:31 -0500

    retesting

--- infra/2-eks-cluster-stage/main.tf
+++ infra/2-eks-cluster-stage/main.tf
@@ -26,7 +26,7 @@ module "eks_cluster" {
   account_number               = data.aws_caller_identity.id_account.id
   vpc_cni_version              = "v1.19.2-eksbuild.1"
   ebs_csi_version              = "v1.34.0-eksbuild.1"
-  pod_identity_version         = "v1.3.4-eksbuild.2"
+  pod_identity_version         = "v1.3.4-eksbuild.1"
   cluster_admins               = "daniel.rivera" # This user will be able to assume the role to manage the cluster
   retention_control_plane_logs = 7
   cluster_enabled_log_types    = ["audit", "api", "authenticator"]
