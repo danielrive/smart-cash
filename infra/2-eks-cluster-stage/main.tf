fd7eee0fffb2e0c72d55708d0bcf9c0348f59c07
danielrive <danielrivera846@gmail.com>
2025-01-12 11:57:37 -0500

    add on pod identity

--- infra/2-eks-cluster-stage/main.tf
+++ infra/2-eks-cluster-stage/main.tf
@@ -35,8 +35,8 @@ module "eks_cluster" {
   instance_type_worker_nodes = var.environment == "develop" ? "t3.medium" : "t3.medium"
   AMI_for_worker_nodes       = "AL2_x86_64"
   desired_nodes              = 3
-  max_instances_node_group   = 4
-  min_instances_node_group   = 2
+  max_instances_node_group   = 3
+  min_instances_node_group   = 3
   storage_nodes              = 20
 }
 
